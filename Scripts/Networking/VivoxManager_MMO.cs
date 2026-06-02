f0ac102efd69e42240edf54d78f02af90ed83c3a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-06-02 14:11:37 +0700

    feat: include `MMOVivoxConfig` to `VivoxManager`

---
+++ Scripts/Networking/VivoxManager_MMO.cs
@@ -0,0 +1,10 @@
+using MultiplayerARPG.MMO;
+using UnityEngine;
+
+namespace Insthync.UnityVivoxIntegration
+{
+    [RequireComponent(typeof(MMOVivoxConfig))]
+    public partial class VivoxManager
+    {
+    }
+}
